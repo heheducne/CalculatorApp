ddf9ae050d5a0292bf7ca30b883776a9486cf1f1
Hoàng Công Danh <115884364+DanhHoang2304@users.noreply.github.com>
2022-10-27 13:24:13 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import reactDom from "react-dom";
-import { StyleSheet, View, Text, TouchableOpacity, Button } from "react-native";
-import {fix_str} from "./calcu.js"
+import { StyleSheet, View, Text, TouchableOpacity, Button, FlatList, TextInput } from "react-native";
+import { fix_str } from "./calcu.js"
+const Item = ({ title }) => (
+  <View>
+    <Text style={styles.hisText}>{title}</Text>
+  </View>
+);
 let count_brackets = 0;
 let flag_num = 0;
+const renderItem = ({ item }) => (
+  <Item title={item.expression + '=' + item.value} />
+);
+
 export default function App() {
   const [resultText, setResultText] = useState("");
   const [calcText, setCalcText] = useState("");
+  const [history, set_history] = useState([]);
+  const [expression, set_expression] = useState('');
+  const [search_history, set_search_history] = useState([...history])
   const onButtonClick = (text) => {
     if (text == "=") {
       return calculateResult();
@@ -15,31 +27,52 @@ export default function App() {
   };
 
   const calculateResult = () => {
-    try{
+    try {
+      let currentHistory = history
+      currentHistory.push({
+        'id': parseInt(history.length) + 1,
+        'expression': resultText,
+        'value': eval(fix_str(resultText)),
+      })
+      set_history(currentHistory)
+      set_search_history(currentHistory)
       setCalcText(eval(fix_str(resultText)))
     }
-    catch(err){
+    catch (err) {
       setCalcText("Syntax error");
     }
   };
+  const renderItem = ({ item }) => (
+    <Item title={item.expression + ' = ' + item.value} />
+  );
 
+
+  const Item = ({ title }) => (
+    <View>
+      <Text style={{ color: 'white', fontSize: 30 }}>{title}</Text>
+    </View>
+  );
   const onOperationClick = (operation) => {
     let operations = ["DEL", "+", "-", "*", "/"];
 
     if (operation == "DEL") {
-      if (resultText.toString().length != 0){
-        if (resultText.slice(-1)=='('){
+      if (resultText.toString().length != 0) {
+        if (resultText.slice(-1) == '(') {
           count_brackets--;
-          if(count_brackets==0) {flag_num=0;
-          // console.log("clgt")}
-        }}
-        if (resultText.slice(-1)==')'){
+          if (count_brackets == 0) {
+            flag_num = 0;
+            // console.log("clgt")}
+          }
+        }
+        if (resultText.slice(-1) == ')') {
           count_brackets++
-          if (count_brackets!=0){flag_num=1;}}
+          if (count_brackets != 0) { flag_num = 1; }
+        }
         return setResultText(
           resultText.toString().substring(0, resultText.length - 1)
         );
-    }}
+      }
+    }
     if (operation == "AC") {
       setResultText("");
       setCalcText(0);
@@ -50,7 +83,20 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      
+      <View style={{ padding: 10 }}>
+        <TextInput placeholder='Find what u want...'
+          style={{ flex: 1, height: 50, fontSize: 30, color: 'white' }}
+          onChangeText={searchString => {
+            var a = history.filter(value => {
+              return (value.expression.includes(
+                searchString) || value.value.toString().includes(searchString)
+              )
+            }
+            )
+            set_search_history(a)
+          }
+          } />
+      </View>
       <View style={styles.result}>
         <Text style={styles.resultText}>{calcText}</Text>
       </View>
@@ -77,51 +123,51 @@ export default function App() {
               <Text style={styles.operationButton}>DEL</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("HIS")}
+            {/* <TouchableOpacity
+              onPress={() => onOperationClick("")}
               style={styles.btn}
             >
-              <Text style={styles.operationButton}>HIS</Text>
+              <Text style={styles.operationButton}>   </Text>
+            </TouchableOpacity> */}
+          </View>
+
+
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => {
+                onButtonClick(1);
+                if (count_brackets != 0) { flag_num = 1; }
+              }}
+              style={styles.btn}
+            >
+              <Text style={styles.number}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                onButtonClick(2);
+                if (count_brackets != 0) { flag_num = 1; }
+              }}
+              style={styles.btn}
+            >
+              <Text style={styles.number}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                onButtonClick(3);
+                if (count_brackets != 0) { flag_num = 1; }
+              }}
+              style={styles.btn}
+            >
+              <Text style={styles.number}>3</Text>
             </TouchableOpacity>
           </View>
 
 
           <View style={styles.row}>
             <TouchableOpacity
-                onPress={() => {
-                  onButtonClick(1);
-                  if (count_brackets!=0) {flag_num = 1;}
-                }}
-                style={styles.btn}
-              >
-                <Text style={styles.number}>1</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  onButtonClick(2);
-                  if (count_brackets!=0) {flag_num = 1;}
-                }}
-                style={styles.btn}
-              >
-                <Text style={styles.number}>2</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => {
-                  onButtonClick(3);
-                  if (count_brackets!=0) {flag_num = 1;}
-                }}
-                style={styles.btn}
-              >
-                <Text style={styles.number}>3</Text>
-              </TouchableOpacity>
-          </View>
-
-
-          <View style={styles.row}>
-          <TouchableOpacity
               onPress={() => {
                 onButtonClick(4);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -130,7 +176,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(5);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -139,7 +185,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(6);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -152,7 +198,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(7);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -161,7 +207,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(8);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -170,7 +216,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(9);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -182,7 +228,7 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 onButtonClick(0);
-                if (count_brackets!=0) {flag_num = 1;}
+                if (count_brackets != 0) { flag_num = 1; }
               }}
               style={styles.btn}
             >
@@ -199,127 +245,127 @@ export default function App() {
             <TouchableOpacity
               onPress={() => {
                 if (flag_num == 0) {
-                  onOperationClick("(") 
+                  onOperationClick("(")
                   count_brackets++;
                 }
                 else {
                   onOperationClick(")")
                   count_brackets--;
-                  if (count_brackets==0) {flag_num = 0;}
-                } 
+                  if (count_brackets == 0) { flag_num = 0; }
+                }
               }}
               style={styles.btn}
             >
               <Text style={styles.number}>( )</Text>
             </TouchableOpacity>
-            
+
           </View>
         </View>
 
         <View style={styles.operations}>
-            <TouchableOpacity
-              onPress={() => {
-                onOperationClick("√(");
-                count_brackets++
-                flag_num=1;
-              }}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>√</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              onOperationClick("√(");
+              count_brackets++
+              flag_num = 1;
+            }}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>√</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("π")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>π</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("π")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>π</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("^")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>^</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("^")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>^</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("!")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>!</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("!")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>!</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onButtonClick("%")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>%</Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity
+            onPress={() => onButtonClick("%")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>%</Text>
+          </TouchableOpacity>
+        </View>
 
 
-          <View style={styles.operations}>
-            <TouchableOpacity
-              onPress={() => onOperationClick("/")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>÷</Text>
-            </TouchableOpacity>
+        <View style={styles.operations}>
+          <TouchableOpacity
+            onPress={() => onOperationClick("/")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>÷</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("*")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>x</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("*")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>x</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("-")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>-</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("-")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>-</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onOperationClick("+")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>+</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => onOperationClick("+")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>+</Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => onButtonClick("=")}
-              style={styles.btn}
-            >
-              <Text style={styles.operationButton}>=</Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity
+            onPress={() => onButtonClick("=")}
+            style={styles.btn}
+          >
+            <Text style={styles.operationButton}>=</Text>
+          </TouchableOpacity>
+        </View>
         <View style={styles.history}>
           <Text style={styles.historytext}>History</Text>
-            {/* <FlatList
+          <FlatList
             data={search_history}
             renderItem={renderItem}
             keyExtractor={item => item.id}
-            /> */}
-          </View>
-
+          />
         </View>
 
-    </View>  
+      </View>
+
+    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"#434343"
+    backgroundColor: "#434343"
   },
   result: {
     flex: 2.5,
     backgroundColor: "grey",
     justifyContent: "center",
     alignItems: "flex-end",
-    borderTopEndRadius:70,
-    borderTopStartRadius:70,
+    borderTopEndRadius: 70,
+    borderTopStartRadius: 70,
 
   },
   calculation: {
@@ -350,7 +396,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-around",
   },
-  history:{
+  history: {
     flex: 2,
     backgroundColor: "grey",
   },
@@ -379,8 +425,13 @@ const styles = StyleSheet.create({
     color: "orange",
     fontWeight: "bold",
   },
-  historytext:{
+  historytext: {
     fontSize: 70,
+    color: "yellow",
+    fontWeight: "bold",
+  },
+  hisText: {
+    fontSize: 20,
     color: "yellow",
     fontWeight: "bold",
   }
